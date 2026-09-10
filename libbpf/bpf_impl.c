76596fecbf5d539850663ad065db290463ab1625
Guy Harris <gharris@sonic.net>
2026-09-10 02:06:51 +0200

    Handle negation in a way that doesn't upset compilers or UBSan.
    Most BPF arithmetic is unsigned, but negation can't be unsigned;
    respecify it as subtracting the value from 0U, so that 1) we don't get
    compiler warnings about negating an unsigned value and 2) don't get
    UBSan warnings about the result of negating 0x80000000 being undefined.
    
    Credit to OSS-Fuzz for finding these issues.

--- libbpf/bpf_impl.c
+++ libbpf/bpf_impl.c
@@ -347,10 +347,13 @@ load_byte:
 			case BPF_ALU|BPF_NEG:
 				/*
 				 * Most BPF arithmetic is unsigned, but negation
-				 * can't be unsigned; throw some casts to
-				 * specify what we're trying to do.
+				 * can't be unsigned; respecify it as subtracting
+				 * the accumulator from 0U, so that 1) we don't
+				 * get compiler warnings about negating an unsigned
+				 * value and 2) don't get UBSan warnings about
+				 * the result of negating 0x80000000 being undefined.
 				 */
-				A = (uint32_t)(-(int32_t)A);
+				A = (0U - A);
 				continue;
 
 			case BPF_MISC|BPF_TAX:
